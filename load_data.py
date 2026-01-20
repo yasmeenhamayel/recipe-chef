d75b741173ff2deb69c9d1e62589d7fdc7a29e28
yasmeenhamayel <yasmeenhamayel41@gmail.com>
2026-01-21 01:56:21 +0200

    Add files via upload

---
+++ load_data.py
@@ -0,0 +1,14 @@
+﻿import pandas as pd
+
+def load_recipes(path):
+    df = pd.read_csv(path)
+    
+    if 'Unnamed: 0' in df.columns:
+        df = df.drop(columns=['Unnamed: 0'])
+    
+    return df
+
+df = load_recipes("recipes.csv")
+def prepare_ingredients(df):
+    df['Cleaned_Ingredients'] = df['Cleaned_Ingredients'].apply(ast.literal_eval)
+    return df
